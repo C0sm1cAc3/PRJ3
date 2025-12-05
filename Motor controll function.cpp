135a3bdfc6d43637d620b77613e9155d8ba5e63e
koen2811-glitch <koen2811@gmail.com>
2025-12-05 20:34:23 +0100

    Update Motor controll function.cpp

--- Motor controll function.cpp
+++ Motor controll function.cpp
@@ -1,1 +1,16 @@
 #include <Arduino.h>
+
+const int motor_A = 1;
+const int motor_B = 2;
+const int motor_C = 3;
+const int motor_D = 4;
+const int forwards = 1; 
+const int backwards = 2; 
+
+void motor_control(int motor, int direction, int power ){
+
+}
+
+void setup(){
+    motor_control(motor_A, forwards, 100); //should send motor A forwards with 100% power
+}
